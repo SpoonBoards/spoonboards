bf6a6f832e811fa38665448aa84cb51631f87d47
devdanlevy <daniel.levy2@iphone.com>
2016-04-05 15:10:37 -0400

    users can only see boards if they are logged in

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,5 +1,7 @@
 class BoardsController < ApplicationController
   before_action :set_board, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate
+
 
   # GET /boards
   # GET /boards.json
