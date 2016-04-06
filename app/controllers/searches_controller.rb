4a8e2fc019e04396f6e7cb8d2bee7ad7cf12aaa1
Dennis Goldin <dennis.goldin@gmail.com>
2016-04-06 10:25:16 -0400

    Fix bugs

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -6,7 +6,7 @@ class SearchesController < ApplicationController
 
   def index
       search_term = params["term"]
-      @new_search = Search.new(search_term)
+      @feature_display = Search.new(search_term)
       @boards = Board.where(user_id: session[:user_id])
   end
 
