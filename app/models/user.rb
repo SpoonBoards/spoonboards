997f2ad932ddaa5a000f0197a65f95a9863eb2ea
LylyG <lgalarza@spoonflower.com>
2016-04-08 15:14:19 -0400

    Password_digest adjustment.

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@ class User < ActiveRecord::Base
       user.provider = auth['provider']
       user.uid = auth['uid']
       user.name = "#{auth[:first_name]}" + " #{auth[:last_name]}"
+      user.password = SecureRandom.hex
       user.save
     end
   end
