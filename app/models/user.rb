b15e3cf9af05289ea8d58e363361109fe632588e
devdanlevy <daniel.levy2@iphone.com>
2016-04-08 14:50:37 -0400

    more slime in re omniauth commenting out user.name in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ActiveRecord::Base
     create! do |user|
       user.provider = auth['provider']
       user.uid = auth['uid']
-      user.name = auth['info']['name']
+      # user.name = auth['info']['name']
       user.save
     end
   end
