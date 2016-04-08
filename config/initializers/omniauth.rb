3a50c175e8604f38e4859e22cfeb1cbe48a7cad4
devdanlevy <daniel.levy2@iphone.com>
2016-04-08 13:37:13 -0400

    change omniauth initializer

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :pinterest, ENV['PINTEREST_APP_ID'], ENV['PINTEREST_APP_SECRET']
+  provider :pinterest, ENV['PINTEREST_APP_ID'], ENV['PINTEREST_APP_SECRET'], provider_ignores_state: true
 end
