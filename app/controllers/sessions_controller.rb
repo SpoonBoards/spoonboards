2b18273913ef6e2df60b3e36338ca0eb73a6cc8f
LylyG <lgalarza@spoonflower.com>
2016-04-09 11:51:11 -0400

    Separate SpoonBoard authentication and omniauth methods in sessions controller.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-  # before_action :set_user, only: [:edit, :update, :delete]
+  before_action :set_user, only: [:edit, :update, :delete]
   before_action :authenticate, except: [:new, :create]
 
   def new
@@ -9,25 +9,29 @@ class SessionsController < ApplicationController
   end
 
   def create
-    # if user = User.find_by_email(params[:email]) && user.authenticate(params[:password])
-    #   session[:user_id] = user.id
-    #   redirect_to boards_path(user), notice: "Signed in!"
+    if user = User.find_by_email(params[:email]) && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to boards_path(user), notice: "Signed in!"
 
+    #  user = User.sign_in_from_omniauth(request.env["omniauth.auth"])
+    #   session[:user_id] = user.id
+    #   redirect_to boards_path, notice: "Signed in!"
+
+    else
+      flash.now[:alert] = "Login failed: invalid email or password."
+      render root_path
+    end
+  end
+
+  def omniauth
      user = User.sign_in_from_omniauth(request.env["omniauth.auth"])
       session[:user_id] = user.id
       redirect_to boards_path, notice: "Signed in!"
 
-    # else
-    #   flash.now[:alert] = "Login failed: invalid email or password."
-    #   render root_path
-    # end
-  end
-
-  def omniauth
-      auth = request.env["omniauth.auth"]
-      user = User.find_by_provider_and_uid(auth["pinterest"], auth["uid"]) || User.create_with_omniauth(auth)
-      session[:user_id] = user.id
-      redirect_to boards_path, :notice => "Signed in!"
+      # auth = request.env["omniauth.auth"]
+      # user = User.find_by_provider_and_uid(auth["pinterest"], auth["uid"]) || User.create_with_omniauth(auth)
+      # session[:user_id] = user.id
+      # redirect_to boards_path, :notice => "Signed in!"
     end
 
   def destroy
