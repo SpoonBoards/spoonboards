8b4da2cc93513d745d0d667ad29c34738bc6cb4c
devdanlevy <daniel.levy2@iphone.com>
2016-04-10 00:53:25 -0400

    work on adding twitter sign in

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,4 +3,13 @@ OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :pinterest, ENV['PINTEREST_APP_ID'], ENV['PINTEREST_APP_SECRET'], provider_ignores_state: true
   provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'], provider_ignores_state: true
+  provider :twitter, ENV["TWITTER_API_KEY"], ENV["TWITTER_API_SECRET"],
+    {
+      :secure_image_url => 'true',
+      :image_size => 'original',
+      :authorize_params => {
+        :force_login => 'true',
+        :lang => 'pt'
+      }
+    }
 end
