828589697db72fa4a47c8e531ab3cc3b05024a74
devdanlevy <daniel.levy2@iphone.com>
2016-04-10 01:36:46 -0400

    twitter amniauth continued configuration

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,7 +3,7 @@ OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :pinterest, ENV['PINTEREST_APP_ID'], ENV['PINTEREST_APP_SECRET'], provider_ignores_state: true
   provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'], provider_ignores_state: true
-  provider :twitter, ENV["TWITTER_API_KEY"], ENV["TWITTER_API_SECRET"],
+  provider :twitter, "IkDH6jCdbyrYkm8i08cC68bKg", "zuOeb1PvAmR5KnE5AID7nHyL0EL2KLWdWpADqTh4MCZypzFYeQ",
     {
       :secure_image_url => 'true',
       :image_size => 'original',
