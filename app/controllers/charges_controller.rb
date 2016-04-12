a77a49fe67204491e3bb6252186aa32e62f983d5
Joseph DePompeis <josephdepompeis@gmail.com>
2016-04-12 16:18:04 -0400

    added algo to prevent purchased items to be calculated

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,13 +1,39 @@
 class ChargesController < ApplicationController
 
   def new
-    @cart_stuff = params[:format]
+    # @cart_stuff = params[:format]
+
+        @cart = Cart.where(user_id: session[:user_id]).first
+
+        @cart_items = CartItem.where(cart_id: @cart.id)
+        @cart_item_ids = []
+        @total_price = []
+        @purchased = []
+        @cart_items.each do |cart_item|
+          if cart_item.purchased == true
+          elsif cart_item.purchased == false
+              cart_item.purchased = 0
+              cart_item.save
+              @total_price << cart_item.price unless cart_item.price == nil
+              @cart_item_ids << cart_item.id
+          end
+        end
+
+
+
+
   end
 
   def create
 
     @cart = Cart.where(user_id: session[:user_id]).first
-    @cart_items = CartItem.where(cart_id: @cart.id)
+
+
+
+
+
+
+
 
     # Amount in cents
 
@@ -25,19 +51,19 @@ class ChargesController < ApplicationController
       :currency    => 'usd'
     )
 
-    @confirmation_details = []
-    @confirmation_details << @charge[:source][:address_line1]
-    @confirmation_details << @charge[:source][:address_line2]
-    @confirmation_details << @charge[:source][:address_city]
-    @confirmation_details << @charge[:source][:address_state]
-    @confirmation_details << @charge[:source][:address_zip]
-    @confirmation_details << @charge[:source][:address_country]
-    @confirmation_details << @charge[:source][:last4]
-    @confirmation_details << @charge[:source][:brand]
-    @confirmation_details << @charge[:amount]
-
-    @purchased_items = []
-    @purchased_items << @cart_items[]
+    # @confirmation_details = []
+    # @confirmation_details << @charge[:source][:address_line1]
+    # @confirmation_details << @charge[:source][:address_line2]
+    # @confirmation_details << @charge[:source][:address_city]
+    # @confirmation_details << @charge[:source][:address_state]
+    # @confirmation_details << @charge[:source][:address_zip]
+    # @confirmation_details << @charge[:source][:address_country]
+    # @confirmation_details << @charge[:source][:last4]
+    # @confirmation_details << @charge[:source][:brand]
+    # @confirmation_details << @charge[:amount]
+    #
+    # @purchased_items = []
+    # @purchased_items << @cart_items[]
 
 
   rescue Stripe::CardError => e
@@ -45,9 +71,4 @@ class ChargesController < ApplicationController
     redirect_to new_charge_path
   end
 
-  # def confirmation_details
-  #   confirmation_details = []
-  #   @charge.each do |r|
-  #     confirmation_details << r.
-  #   end
 end
