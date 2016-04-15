de6c515c36c50c0297a261bf024c757a461ee380
LylyG <lgalarza@spoonflower.com>
2016-04-15 08:40:21 -0400

    More SF api testing and setup.

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -3,7 +3,7 @@ class ChargesController < ApplicationController
   def new
     @cart = Cart.where(user_id: session[:user_id]).first
     @amount = @cart.calculate_price_based_on_qty
-
+    @cart.as_json
   end
 
 
@@ -24,21 +24,18 @@ class ChargesController < ApplicationController
       :receipt_email => customer.email
     )
 
-    # Insert API call here dawg
-    # @result = HTTParty.post("https://api-dev.sproutpatterns.com:9517/order/create"),
-    #     :body => { :fabric_id => 'This is the fabric id',
-    #                :user_id => 'SpoonBoards user_id',
-    #                :design_id => '111111',
-    #                :app_id => 2,
-    #              }.to_json,
-    #
-    #     :headers => {"X-Auth-Token" => "#{ENV["SPOON_KEY"]}"
-    #
-    # receipt_details = @cart.prepare_receipt_details(@amount, @charge)
-    # @receipt = @cart.create_receipt(receipt_details)
-    # @cart.mark_cart_items_purchased(@receipt.id)
+    @sf_order = HTTParty.post("https://api-dev.sproutpatterns.com:9517/order/create/",
+        :body => [{ :fabric_id => 4,
+                   :user_id => 1857556,
+                   :design_id => 5055154,
+                   :app_id => 2
+                 }].to_json,
+        :headers => {'X-Auth-Token' => "#{ENV["SPOON_KEY"]}", 'Content-Type' => 'application/json'})
 
 
+    receipt_details = @cart.prepare_receipt_details(@amount, @charge)
+    @receipt = @cart.create_receipt(receipt_details)
+    @cart.mark_cart_items_purchased(@receipt.id)
 
 
   rescue Stripe::CardError => e
