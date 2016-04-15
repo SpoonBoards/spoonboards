ebb6a5ddf577975366ec59bd679a176900bb44f1
Joseph DePompeis <josephdepompeis@gmail.com>
2016-04-14 22:18:43 -0400

    commented out troublesome lines

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -24,15 +24,15 @@ class ChargesController < ApplicationController
       :receipt_email => customer.email
     )
 
-    # Insert API call here dawg
-    @result = HTTParty.post("https://api-dev.sproutpatterns.com:9517/order/create"),
-        :body => { :fabric_id => 'This is the fabric id',
-                   :user_id => 'SpoonBoards user_id',
-                   :design_id => '111111',
-                   :app_id => 2,
-                 }.to_json,
-
-        :headers => {"X-Auth-Token" => "#{ENV["SPOON_KEY"]}"
+    # # Insert API call here dawg
+    # @result = HTTParty.post("https://api-dev.sproutpatterns.com:9517/order/create"),
+    #     :body => { :fabric_id => 'This is the fabric id',
+    #                :user_id => 'SpoonBoards user_id',
+    #                :design_id => '111111',
+    #                :app_id => 2,
+    #              }.to_json,
+    #
+    #     :headers => {"X-Auth-Token" => "#{ENV["SPOON_KEY"]}"
 
     receipt_details = @cart.prepare_receipt_details(@amount, @charge)
     @receipt = @cart.create_receipt(receipt_details)
