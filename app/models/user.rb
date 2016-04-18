c19deb2e02df5d4a8fc9544e64495aea5fbe7eb2
devdanlevy <daniel.levy2@iphone.com>
2016-04-18 15:31:41 -0400

    giving up on name field

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ActiveRecord::Base
     create! do |user|
       user.provider = auth['provider']
       user.uid = auth['uid']
-      user.name = auth['name'] || "#{auth['first_name']} #{auth['last_name']}"
+      user.name = auth
       user.password = SecureRandom.hex
       user.save
       Cart.create!(user_id: user.id)
