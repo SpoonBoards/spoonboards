2aaa8842bf657f19d87c8ebc0afd1fcf6329490a
Joseph DePompeis <josephdepompeis@gmail.com>
2016-04-19 00:13:37 -0400

    ajax functionality to user edits

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,7 +40,9 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.html { redirect_to users_path, notice: 'User was successfully updated.' }
+        format.js   {}
+
         format.json { render :show, status: :ok, location: @user }
       else
         format.html { render :edit }
